0931afb49a390ca01cf55465c84a5c0051ae315d
RickDeGroot9003557 <r.degroot02@hotmail.nl>
2025-06-25 11:34:25 +0200

    fix with cors

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, apikey');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    next();
+    res.sendStatus(204);
 });
 
 app.use((req, res, next) => {
